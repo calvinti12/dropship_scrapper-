c947d4e99c439ceb53bb70c0d8c03757c7665d67
Ben Goffer <ben@fans-league.club>
2020-06-03 10:09:09 +0300

    Done with evaluate

--- Frontpages/evaluate.py
+++ Frontpages/evaluate.py
@@ -1,10 +1,15 @@
+from selenium import webdriver
 from flask import Flask, render_template
+
+driver = None
+
 niche_list = ["Pets",
               "House improvements",
               "Security",
               "Beauty & pleasure",
               "Personal health",
               "Babies & Kids",
+              "Fashion",
               "Fitness Workout",
               "Electronics & Gadgets",
               "Jewels & Watches"]
@@ -18,17 +23,21 @@ def fix_url(url):
     return fixed_url.rstrip('/')
 
 
-class Evaluate:
-    def __init__(self):
-        pass
+def open_site(site):
+    global driver
+    if driver is None:
+        options = webdriver.ChromeOptions()
+        # options.add_argument("headless")
+        driver = webdriver.Chrome(executable_path=r"chromedriver.exe", chrome_options=options)
 
-    def open_site(self, site):
-        link = fix_url(site['link'])
-        return render_template('template.html',
-                               link=link,
-                               data_link=site['link'],
-                               number_of_products=str(site['number_of_product']),
-                               last_product_updated=str(site['last_product_updated']),
-                               first_product_published=str(site['first_product_published']),
-                               niche_list=niche_list)
+    link = fix_url(site['link'])
+    driver.get(link)
+    return render_template('template.html',
+                           link=link,
+                           data_link=site['link'],
+                           number_of_products=str(site['number_of_product']),
+                           last_product_updated=str(site['last_product_updated']).replace(' 00:00:00', '')[::-1],
+                           first_product_published=str(site['first_product_published']).replace(' 00:00:00', '')[::-1],
+                           niche_list=niche_list)
+
 
--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from Google.google_sheets import GoogleSheets
 from Database.atlas import MongoAtlas
-from Frontpages.evaluate import Evaluate
+from Frontpages.evaluate import open_site
 from Scrappers.awis_api_wrapper import get_rank
 from Google.google_function import get_store_products
 from Google.google_function import scrape_my_ips
@@ -10,26 +10,23 @@ from flask import Flask, request, jsonify
 # gts = GoogleTrends(["Acupressure Relief Mat"])
 sites_sheet = GoogleSheets('Sites & products')
 atlas = MongoAtlas()
-evaluate = Evaluate()
+
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-@app.route("/Evaluate")
-def evaluate_site():
-    return evaluate.open_site(atlas.get_site_to_evaluate())
-
-
-@app.route("/next_site", methods=['POST'])
-def next_site():
-    data = request.form.to_dict(flat=False)
-    if data:
-        atlas.evaluate_site(data['data_link'][0],
-                            bool(data['is_dropshipper'][0]),
-                            data['niche'][0],
-                            "Test",
-                            bool(data['is_branded_products'][0]),
-                            int(data['our_ranking'][0]))
-    return evaluate.open_site(atlas.get_site_to_evaluate())
+@app.route("/evaluate", methods=['GET', 'POST'])
+def evaluate():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=False)
+        if data:
+            atlas.evaluate_site(data['data_link'][0],
+                                eval(data['is_dropshipper'][0]),
+                                data['niche'][0],
+                                data['main_product'][0],
+                                eval(data['is_branded_products'][0]),
+                                int(data['our_ranking'][0]))
+    return open_site(atlas.get_site_to_evaluate())
 
 
 @app.route("/Start_update")
