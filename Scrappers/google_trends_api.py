f9da4022a3052be2848d804589549130f22df950
Ben Goffer <goffer14@gmail.com>
2020-06-04 12:37:58 +0300

    fix get facebook ads & remove req

--- Scrappers/google_trends_api.py
+++ Scrappers/google_trends_api.py
@@ -1,4 +1,3 @@
-import pandas as pd                        
 from pytrends.request import TrendReq
 # pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1)
 pytrend = TrendReq()
